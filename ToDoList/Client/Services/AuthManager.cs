5b8edb2fd10c8613a42ba0afc7e8eba5815ffec4
Kurulko <kurul300013@gmail.com>
2022-10-30 15:29:43 +0200

    Change HttpClient's path

--- ToDoList/Client/Services/AuthManager.cs
+++ ToDoList/Client/Services/AuthManager.cs
@@ -13,7 +13,7 @@ namespace ToDoList.Client.Services
         public async Task<CurrentUser> CurrentUserInfo()
             => await new HttpClient().GetFromJsonAsync<CurrentUser>(baseApiUrl + "CurrentUserInfo");
             //=> await httpClient.GetFromJsonAsync<CurrentUser>(baseApiUrl + "CurrentUserInfo");
-
+        
         public async Task LoginUserAsync(LoginModel model)
             => await CheckResponseMessage(await httpClient.PostAsJsonAsync(baseApiUrl + "login", model));
 
